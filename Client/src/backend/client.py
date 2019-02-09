45d97ff1a2b762413f79a9ad9f60442369709525
Joshua Comish <jcomish@sourceiron.com>
2019-02-09 15:38:39 -0600

    JC - One more fix

--- Client/src/backend/client.py
+++ Client/src/backend/client.py
@@ -9,8 +9,6 @@ gui_dir = os.path.join(os.getcwd(), "gui")  # development path
 if not os.path.exists(gui_dir):  # frozen executable path
     gui_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "gui")
 
-print
-
 client = Flask(__name__, static_folder=gui_dir, template_folder=gui_dir)
 client.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1  # disable caching
 
